1608e846b1fab0a22dae26f84edd6ecd8cee664a
Giorf81 <rejroberts7@gmail.com>
2024-02-16 14:01:19 +0300

    Добавьте файлы проекта.

---
+++ массивы/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace массивы
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите свое имя");
+            var imya = Console.ReadLine();
+
+            Console.WriteLine("Ваше имя по буквам в обратном порядке:");
+
+            for(int i = imya.Length-1; i >=0; i--)
+            {
+                Console.Write(imya[i] + " ");
+            } 
+            Console.WriteLine("Последняя буква вашего имени: {0}",
+                imya[imya.Length - 1]);
+
+            Console.ReadKey();
+
+        }
+    }
+}
